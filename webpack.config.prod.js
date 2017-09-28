8cc97a236582bc40aa0bc21769b11dac4e7e3002
Virendra Singh Pawar <virendrasinghpawar@gmail.com>
2017-09-28 11:37:25 +0530

    host changed

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,7 +25,7 @@ export default {
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
-        'API_HOST': JSON.stringify('https://code-pair-api.herokuapp.com')
+        'API_HOST': JSON.stringify('https://paircode.herokuapp.com')
       }
     })
   ],
